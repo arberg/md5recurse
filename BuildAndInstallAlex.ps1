304d5219d1cc7e9786762f437fd5384f685fb55c
Alex Rune Berg <arberg.bnr@gmail.com>
2018-10-13 10:51:06 +0200

    scripts

---
+++ BuildAndInstallAlex.ps1
@@ -0,0 +1,4 @@
+.\gradlew build
+Expand-Archive d:\Development\GitHub\md5recurse\build\distributions\md5recurse.zip  -DestinationPath d:\Development\BnrTools -force
+Expand-Archive d:\Development\GitHub\md5recurse\build\distributions\md5recurse.zip  -DestinationPath \\tower\flash\app -force
+Expand-Archive d:\Development\GitHub\md5recurse\build\distributions\md5recurse.zip  -DestinationPath c:\Tools -force
